878afcb5be5616993b68d40b17bfa6498a95126a
Axel Hégron <axelhegron@gmail.com>
2025-11-23 11:58:02 +0100

    styled index.vue

--- front/app/pages/index.vue
+++ front/app/pages/index.vue
@@ -1,7 +1,31 @@
 <template>
-    <h1>Welcome to the homepage</h1>
-    <CharacterUploader />
+    <UContainer class="container min-h-[calc(100vh-var(--ui-header-height))]">
+        <UCard variant="subtle" class="container card">
+            <UContainer class="container">
+                <CharacterUploader />
+            </UContainer>
+
+        </UCard>
+    </UContainer>
 </template>
 
-<script setup lang="ts">
-</script>
+<style scoped>
+.container {
+    display: flex;
+    align-self: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+}
+
+.card {
+    height: 80lvh;
+}
+
+:deep(.card > div) {
+    display: flex;
+    align-self: center;
+    justify-content: center;
+    cursor: pointer;
+}
+</style>
