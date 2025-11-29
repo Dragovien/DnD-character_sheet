35b9be74a02ab23a34ba72ea59c124ceb98b5699
Axel Hégron <axelhegron@gmail.com>
2025-11-29 16:03:14 +0100

    retrieved data

--- front/app/pages/personnage.vue
+++ front/app/pages/personnage.vue
@@ -48,7 +48,63 @@
         </UCard>
 
 
+        <UCard>
+            <template #header>
+                <h3 class="text-xl font-semibold mt-4">💠 Combat</h3>
+            </template>
 
+            <div class="grid grid-cols-3 gap-4">
+                <div><strong>Bonus de maitrise :</strong> {{ store.character.pb }}</div>
+                <div><strong>CA:</strong> {{ store.character.ac }}</div>
+                <div><strong>Initiative :</strong> {{ store.character.init }}</div>
+                <div><strong>Vitesse :</strong> {{ store.character.speed }}</div>
+                <div><strong>Taille :</strong> {{ store.character.size }}</div>
+                <div><strong>Perception passive :</strong> {{ store.character.passive }}</div>
+
+                <div><strong>HP par level:</strong> {{ store.character.hpFormula }}</div>
+                <div><strong>HP actuels:</strong> {{ store.character.hpCurrent }}</div>
+                <div><strong>HP temp :</strong> {{ store.character.hpTemp }}</div>
+                <div><strong>HP max :</strong> {{ store.character.hpMax }}</div>
+                <div>
+                    Dés de vie
+                    <div><strong>Dépensés :</strong> {{ store.character.hitDiceSpent }}</div>
+                    <div><strong>Max :</strong> {{ store.character.hitDiceMax }}</div>
+                </div>
+
+                <div>
+                    Training Mastery
+                    <div>
+                        Armure
+                        <div><strong>Légère :</strong> {{ store.character.masteryTraining.armors.light ? 'Oui' : 'Non'
+                            }}</div>
+                        <div><strong>Intermédiaire :</strong> {{ store.character.masteryTraining.armors.intermediate ?
+                            'Oui' :
+                            'Non' }}</div>
+                        <div><strong>Lourde :</strong> {{ store.character.masteryTraining.armors.heavy ? 'Oui' : 'Non'
+                            }}</div>
+                        <div><strong>Bouclier :</strong> {{ store.character.masteryTraining.armors.shield ? 'Oui' :
+                            'Non' }}
+                        </div>
+                    </div>
+                    <div>
+                        Armes
+                        <div>{{ store.character.masteryTraining.weapons }}</div>
+                    </div>
+                    <div>
+                        Outils
+                        <p class="whitespace-pre-line">{{ store.character.masteryTraining.tools }}</p>
+                    </div>
+                </div>
+
+                <div>
+                    Jets contre la mort
+                    <p>Succès: <span>{{ addDeathSavings(successThrows) }}</span>/3</p>
+                    <p>Echecs: <span>{{ addDeathSavings(failThrows) }}</span>/3</p>
+                </div>
+
+                <div>Inspiration {{ store.character.inspiration ? 'Oui' : 'Non' }}</div>
+            </div>
+        </UCard>
 
         <!-- APPARENCE & HISTOIRE -->
         <UCard>
@@ -92,6 +148,23 @@ const formatMod = (v: number | undefined) => {
     if (v === undefined || v === null) return ''
     return v >= 0 ? `+${v}` : `-${v}`
 }
+
+const successThrows = computed(() => [
+    Boolean(store.character?.deathSaving.success1),
+    Boolean(store.character?.deathSaving.success2),
+    Boolean(store.character?.deathSaving.success3)
+])
+
+const failThrows = computed(() => [
+    Boolean(store.character?.deathSaving.fail1),
+    Boolean(store.character?.deathSaving.fail2),
+    Boolean(store.character?.deathSaving.fail3)
+])
+
+const addDeathSavings = (array: boolean[]): number => {
+    return array.filter(v => v).length
+}
+
 </script>
 
 <style scoped></style>
