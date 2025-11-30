a053a9a0647a254cacf599a9315ca2854217dc12
Axel Hégron <axelhegron@gmail.com>
2025-11-30 15:41:20 +0100

    updated training

--- front/app/pages/personnage/entrainements_maitrises.vue
+++ front/app/pages/personnage/entrainements_maitrises.vue
@@ -3,33 +3,35 @@
         <!-- Training Mastery -->
         <UCard>
             <template #header>
-                <h3 class="text-xl font-semibold mt-4">Entrainements et maitrises</h3>
+                <h3 class="text-xl font-semibold mt-4">
+                    <UIcon name="game-icons:master-of-arms" class="size-5" /> Entrainements et maitrises
+                </h3>
             </template>
 
             <div class="grid grid-cols-3 gap-4">
-
                 <div>
-                    <div>
-                        Armure
-                        <div><strong>Légère :</strong> {{ store.character.masteryTraining.armors.light ? 'Oui' : 'Non'
-                            }}</div>
-                        <div><strong>Intermédiaire :</strong> {{ store.character.masteryTraining.armors.intermediate ?
-                            'Oui' :
-                            'Non' }}</div>
-                        <div><strong>Lourde :</strong> {{ store.character.masteryTraining.armors.heavy ? 'Oui' : 'Non'
-                            }}</div>
-                        <div><strong>Bouclier :</strong> {{ store.character.masteryTraining.armors.shield ? 'Oui' :
-                            'Non' }}
-                        </div>
+                    <strong>Armure</strong>
+                    <USeparator class="separator" />
+                    <div><strong>Légère :</strong> {{ store.character.masteryTraining.armors.light ? 'Oui' : 'Non'
+                        }}</div>
+                    <div><strong>Intermédiaire :</strong> {{ store.character.masteryTraining.armors.intermediate ?
+                        'Oui' :
+                        'Non' }}</div>
+                    <div><strong>Lourde :</strong> {{ store.character.masteryTraining.armors.heavy ? 'Oui' : 'Non'
+                        }}</div>
+                    <div><strong>Bouclier :</strong> {{ store.character.masteryTraining.armors.shield ? 'Oui' :
+                        'Non' }}
                     </div>
-                    <div>
-                        Armes
-                        <div>{{ store.character.masteryTraining.weapons }}</div>
-                    </div>
-                    <div>
-                        Outils
-                        <p class="whitespace-pre-line">{{ store.character.masteryTraining.tools }}</p>
-                    </div>
+                </div>
+                <div>
+                    <strong>Armes</strong>
+                    <USeparator class="separator" />
+                    <div>{{ store.character.masteryTraining.weapons }}</div>
+                </div>
+                <div>
+                    <strong>Outils</strong>
+                    <USeparator class="separator" />
+                    <p class="whitespace-pre-line">{{ store.character.masteryTraining.tools }}</p>
                 </div>
             </div>
         </UCard>
